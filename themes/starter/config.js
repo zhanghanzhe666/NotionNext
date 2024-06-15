b2e1e2a3b3b03e47aee4f16ea82ab3d8534d8df3
zhanghanzhe666 <146498764+zhanghanzhe666@users.noreply.github.com>
2024-06-15 08:16:31 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   STARTER_HERO_BUTTON_2_URL: 'https://github.com/zhanghanzhe666/bingo', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
 
-  STARTER_HERO_PREVIEW_IMAGE: 'https://github.com/zhanghanzhe666/NotionNext/blob/run/themes/starter/img/bingo.png', // 产品预览图 ，默认读取public目录下图片
+  // STARTER_HERO_PREVIEW_IMAGE: 'https://github.com/zhanghanzhe666/NotionNext/blob/run/themes/starter/img/bingo.png', // 产品预览图 ，默认读取public目录下图片
 
   // 顶部右侧导航暗流
   STARTER_NAV_BUTTON_1_TEXT: 'Sign In',
